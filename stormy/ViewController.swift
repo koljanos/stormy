864bf4f1634a97cddbc837517a1d8630973a1816
Николай Белиовский <bohrock@yandex.ru>
2018-11-02 23:43:31 +0300

    Initial Commit

---
+++ stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stormy
+//
+//  Created by Николай Белиовский on 02/11/2018.
+//  Copyright © 2018 Николай Белиовский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
