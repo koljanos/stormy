cec37f23086b76d886cfa46abfbe829070b2950c
Николай Белиовский <bohrock@yandex.ru>
2018-11-04 21:16:31 +0300

    wrap up and its done

--- stormy/DetailViewController.swift
+++ stormy/DetailViewController.swift
@@ -18,8 +18,19 @@ class DetailViewController: UIViewController {
             imageView.image  = UIImage(named: imageToLoad)
         }
         // Do any additional setup after loading the view.
+        title = selectedImage
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- stormy/ViewController.swift
+++ stormy/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		navigationController?.navigationBar.prefersLargeTitles = true
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -26,6 +27,8 @@ class ViewController: UITableViewController {
             }
         }
 		print(pictures)
+		title = "Storm Viewer"
+		
     }
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
