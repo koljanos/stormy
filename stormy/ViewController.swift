8d27831bb6a25644501d02baf5e0a14d10f5900d
Николай Белиовский <bohrock@yandex.ru>
2018-11-04 21:09:22 +0300

    added UIImage draw

--- stormy/ViewController.swift
+++ stormy/ViewController.swift
@@ -41,6 +41,17 @@ class ViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		// 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+		if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+			// 2: success! Set its selectedImage property
+			vc.selectedImage = pictures[indexPath.row]
+			
+			// 3: now push it onto the navigation controller
+			navigationController?.pushViewController(vc, animated: true)
+		}
+	}
 
 
 }
